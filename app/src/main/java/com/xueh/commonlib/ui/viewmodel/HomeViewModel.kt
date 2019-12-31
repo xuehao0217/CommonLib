a0c55be214b38ad844ccd14786499b957d57b7c3
xueh <644738532@qq.com>
2019-12-31 16:32:48 +0800

    mvvm封装

--- app/src/main/java/com/xueh/commonlib/ui/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/xueh/commonlib/ui/viewmodel/HomeViewModel.kt
@@ -1,8 +1,8 @@
 package com.xueh.commonlib.ui.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import com.xueh.comm_core.base.mvvm.BaseViewModel
 import com.xueh.comm_core.net.ServiceGenerator
-import com.xueh.comm_core.net.livedata.StateLiveData
 import com.xueh.commonlib.api.RestApi
 import com.xueh.commonlib.entity.BannerVO
 
@@ -14,12 +14,12 @@ import com.xueh.commonlib.entity.BannerVO
 class HomeViewModel : BaseViewModel<RestApi>() {
     override fun initApi() = ServiceGenerator.getService(RestApi::class.java)
 
-    val stateBanner = StateLiveData<List<BannerVO>?>()
+    val stateBanner = MutableLiveData<List<BannerVO>?>()
 
     fun loadData() {
         launchOnUI {
             //  stateBanner.postValueAndSuccess(api.bannerList2().await().data)
-            stateBanner.postValueAndSuccess(api.bannerList3().data)
+            stateBanner.postValue(api.bannerList3().data)
         }
     }
 }
