4702859e04ea68c906b6d305de3af667485d6a2a
xuehao <xuehao@soyoung.com>
2021-05-20 11:56:21 +0800

    添加计时器

--- app/src/main/java/com/xueh/commonlib/ui/MyFragment.kt
+++ app/src/main/java/com/xueh/commonlib/ui/MyFragment.kt
@@ -6,9 +6,11 @@ import android.os.Bundle
 import com.fengchen.uistatus.annotation.UiStatus
 import com.xueh.comm_core.base.DFragment
 import com.xueh.comm_core.utils.TakePictureUtils
+import com.xueh.comm_core.utils.time.Interval
 import com.xueh.commonlib.R
 import com.xueh.commonlib.databinding.FragmentMyBinding
 import java.io.File
+import java.util.concurrent.TimeUnit
 
 
 /**
@@ -33,6 +35,12 @@ class MyFragment : DFragment<FragmentMyBinding>() {
             tvContent.setOnClickListener {
                 showState(UiStatus.CONTENT)
             }
+            btStartTime.setOnClickListener {
+                interval.resume()
+            }
+            btStartPause.setOnClickListener {
+                interval.pause()
+            }
         }
 
     }
@@ -40,6 +48,7 @@ class MyFragment : DFragment<FragmentMyBinding>() {
     override fun initDataAfterView() {
     }
 
+    private lateinit var interval: Interval
     override fun initView(savedInstanceState: Bundle?) {
         bindStateView(binding.ivMy)
         takePictureUtils =
@@ -51,6 +60,21 @@ class MyFragment : DFragment<FragmentMyBinding>() {
                     binding.ivMy.setImageURI(Uri.fromFile(outFile?.absoluteFile))
                 }
             })
+
+
+
+        interval = Interval(
+            0,
+            1,
+            TimeUnit.SECONDS,
+            30
+        ).life(this) // 自定义计数器个数的轮循器, 当[start]]比[end]值大, 且end不等于-1时, 即为倒计时
+        // interval = Interval(1, TimeUnit.SECONDS) // 每秒回调一次, 不会自动结束
+        interval.subscribe {
+            binding.tvTime.text = it.toString()
+        }.finish {
+            binding.tvTime.text = "计时完成"
+        }.start()
     }
 
     override fun onRequestPermissionsResult(
