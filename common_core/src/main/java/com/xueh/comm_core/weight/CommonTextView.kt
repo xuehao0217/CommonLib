c32b1ba5ba55eda3c3c3bd7ea0f3bb2ac0d380e0
xuehao <xuehao@deepway.ai>
2022-06-28 12:43:21 +0800

    升级版本

--- common_core/src/main/java/com/xueh/comm_core/weight/CommonTextView.kt
+++ common_core/src/main/java/com/xueh/comm_core/weight/CommonTextView.kt
@@ -2,18 +2,39 @@ package com.xueh.comm_core.weight
 
 import android.content.Context
 import android.util.AttributeSet
-import androidx.compose.ui.graphics.Color
-import com.blankj.utilcode.util.ResourceUtils
+import androidx.annotation.ColorRes
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.appcompat.widget.TintTypedArray
+import androidx.core.view.ViewCompat
 import com.google.android.material.button.MaterialButton
 import com.xueh.comm_core.R
+import com.xueh.comm_core.helper.px
 
 class CommonTextView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : MaterialButton(context, attrs, R.style.common_textview_style) {
     init {
-        setBackgroundColor(R.color.transparent)
-        setRippleColorResource(R.color.transparent)
         insetBottom = 0
         insetTop = 0
+        backgroundTintList =
+            AppCompatResources.getColorStateList(context, R.color.white)
+
     }
+
+    fun setOutLineCircleShape(
+        cornerRadius: Int,
+        strokeWidth: Int,
+        @ColorRes strokeColorResourceId: Int
+    ) {
+        this.cornerRadius = cornerRadius.toFloat().px()
+        this.strokeWidth = strokeWidth.toFloat().px()
+        setStrokeColorResource(strokeColorResourceId)
+    }
+
+    fun setCircleShape(cornerRadius: Int, @ColorRes strokeColorResourceId: Int) {
+        this.cornerRadius = cornerRadius.toFloat().px()
+        backgroundTintList =
+            AppCompatResources.getColorStateList(context, strokeColorResourceId)
+    }
+
 }
