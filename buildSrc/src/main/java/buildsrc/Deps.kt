ab41bf510d68a50b3d64023425e1693eee7d040d
xuehao <xuehao@deepway.ai>
2022-07-21 10:43:16 +0800

    优化代码

--- buildSrc/src/main/java/buildsrc/Deps.kt
+++ buildSrc/src/main/java/buildsrc/Deps.kt
@@ -176,6 +176,9 @@ object Deps {
             "com.google.accompanist:accompanist-swiperefresh:${Version.accompanistVersion}"
 
         const val compose_refreshlayout = "io.github.shenzhen2017:compose-refreshlayout:1.0.0"
+
+        const val customview="androidx.customview:customview:1.2.0-alpha01"
+        const val customview_poolingcontainer = "androidx.customview:customview-poolingcontainer:1.0.0-alpha01"
     }
 
 
