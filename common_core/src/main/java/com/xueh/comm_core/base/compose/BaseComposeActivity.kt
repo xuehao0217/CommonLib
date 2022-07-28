7945823bd41db75abb30dd3a2c58c0113489b1dc
xuehao <xuehao@deepway.ai>
2022-07-28 11:36:15 +0800

    优化代码

--- common_core/src/main/java/com/xueh/comm_core/base/compose/BaseComposeActivity.kt
+++ common_core/src/main/java/com/xueh/comm_core/base/compose/BaseComposeActivity.kt
@@ -2,14 +2,16 @@ package com.xueh.comm_core.base.compose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.xueh.comm_core.base.IBaseLogic
 import com.xueh.comm_core.utils.compose.hideIme
 import com.xueh.comm_core.utils.compose.setAndroidNativeLightStatusBar
 import com.xueh.comm_core.utils.compose.showIme
 import com.xueh.comm_core.utils.compose.transparentStatusBar
 
-
-abstract class BaseComposeActivity : ComponentActivity(), IBaseLogic {
+//ComponentActivity
+//AppCompatActivity
+abstract class BaseComposeActivity : AppCompatActivity(), IBaseLogic {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView(savedInstanceState)
