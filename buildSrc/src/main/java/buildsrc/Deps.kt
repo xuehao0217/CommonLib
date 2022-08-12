db25526d356c055ca63f164fea31db2617f74125
xuehao <xuehao@deepway.ai>
2022-08-12 16:28:48 +0800

    添加镜像

--- buildSrc/src/main/java/buildsrc/Deps.kt
+++ buildSrc/src/main/java/buildsrc/Deps.kt
@@ -45,7 +45,7 @@ object Deps {
 
         const val immersionbar = "3.0.0"
         const val utilcode = "1.31.0"
-        const val BaseRecyclerViewAdapterHelper = "3.0.6"
+        const val BaseRecyclerViewAdapterHelper = "3.0.7"
         const val walle = "1.1.6"
         const val titlebar = "8.0"
         const val UiStatus = "1.0.5"
@@ -180,10 +180,13 @@ object Deps {
         const val customview="androidx.customview:customview:1.2.0-alpha01"
         const val customview_poolingcontainer = "androidx.customview:customview-poolingcontainer:1.0.0-alpha01"
 
+
+        const val lifecycle_runtime_ktx =  "androidx.lifecycle:lifecycle-runtime-ktx:2.5.0-rc01"
         //获取viewmodel
         // import androidx.lifecycle.viewmodel.compose.viewModel
-        // val viewModel1: ComposeViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
-        const val lifecycle_runtime_ktx =  "androidx.lifecycle:lifecycle-runtime-ktx:2.5.0-rc01"
+        //val viewModel1: ComposeViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
+        const val lifecycle_viewmodel =  "androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1"
+
     }
 
 
