a27dedaeeeddd36db7f7a29970f16e065f85149c
xuehao <xuehao@deepway.ai>
2022-08-30 21:23:20 +0800

    添加文档注释

--- app/src/main/java/com/xueh/commonlib/ui/viewmodel/ComposeViewModel.kt
+++ app/src/main/java/com/xueh/commonlib/ui/viewmodel/ComposeViewModel.kt
@@ -4,11 +4,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.MutableLiveData
+import androidx.paging.PagingData
 import com.xueh.comm_core.base.mvvm.BaseViewModel
 import com.xueh.comm_core.helper.pager
 import com.xueh.comm_core.net.HttpRequest
 import com.xueh.commonlib.api.RestApi
 import com.xueh.commonlib.entity.BannerVO
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class ComposeViewModel : BaseViewModel<RestApi>() {
@@ -37,7 +39,7 @@ class ComposeViewModel : BaseViewModel<RestApi>() {
         }
     }
 
-
+//    var msgDatas by mutableStateOf<Flow<PagingData<HomeEntity>>?>(null)
     fun getListDatas() = pager { api.getHome(it).data.datas }
 }
 
