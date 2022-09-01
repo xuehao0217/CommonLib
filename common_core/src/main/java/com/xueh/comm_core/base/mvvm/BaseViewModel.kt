baf625130a14b60acbb3e4dbc63728290b96b58f
xuehao <xuehao@deepway.ai>
2022-09-01 17:37:54 +0800

    新增取消上一次网络请求协程

--- common_core/src/main/java/com/xueh/comm_core/base/mvvm/BaseViewModel.kt
+++ common_core/src/main/java/com/xueh/comm_core/base/mvvm/BaseViewModel.kt
@@ -1,9 +1,15 @@
 package com.xueh.comm_core.base.mvvm
 
 import android.util.Log
+import androidx.lifecycle.viewModelScope
+import com.blankj.utilcode.util.LogUtils
 import com.xueh.comm_core.net.BaseResult
 import com.xueh.comm_core.net.coroutinedsl.RequestViewModel
 import com.xueh.comm_core.net.coroutinedsl.ViewModelDsl
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 
 /**
@@ -40,6 +46,7 @@ abstract class BaseViewModel<E> : RequestViewModel() {
         }
     }
 
+    //DSL
     protected fun <Response> apiDslBaseResult(
         apiDSL: ApiDslBaseResult<Response>.() -> Unit
     ) {
@@ -70,4 +77,39 @@ abstract class BaseViewModel<E> : RequestViewModel() {
         }
     }
 
+
+    //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    protected var jobs = hashMapOf<String, Job>()
+    //开启协程 如果有上一个任务 则取消
+    //key 协程的 key
+    fun launchCancelLast(key:String, block: suspend CoroutineScope.() -> Unit){
+        jobs.get(key)?.let {
+            it.cancel()
+        }
+        jobs.put(key, launch{
+            block.invoke(this)
+        })
+    }
+
+    //安全开启协程
+    fun launch(
+        showLoading:Boolean=true,
+        onError: (suspend ((Throwable) -> Unit))? = null,
+        block: suspend CoroutineScope.() -> Unit,
+    ) = viewModelScope.launch(CoroutineExceptionHandler { coroutineContext, throwable ->
+        apiException.value = throwable
+        LogUtils.eTag("BaseViewModel", "BaseViewModel CoroutineException --》${throwable}")
+    }) {
+        apiLoading.value = showLoading
+        kotlin.runCatching {
+            block.invoke(this)
+        }.onFailure {
+            //为了解决 suspend
+            apiException.value = it
+            onError?.invoke(it)
+            LogUtils.eTag("BaseViewModel", "BaseViewModel try catch --》${it}")
+        }
+        apiLoading.value = false
+    }
 }
