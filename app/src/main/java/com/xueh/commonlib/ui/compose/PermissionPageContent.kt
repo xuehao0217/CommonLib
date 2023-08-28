67a91d0810286bcdaae730b8a7c3b0d6bfb97e42
xuehao <xuehao@deepway.ai>
2023-08-28 18:12:15 +0800

    优化代码

--- app/src/main/java/com/xueh/commonlib/ui/compose/PermissionPageContent.kt
+++ app/src/main/java/com/xueh/commonlib/ui/compose/PermissionPageContent.kt
@@ -2,12 +2,15 @@ package com.xueh.commonlib.ui.compose
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.blankj.utilcode.util.ToastUtils
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionStatus
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.accompanist.permissions.rememberPermissionState
+import com.xueh.comm_core.helper.compose.rememberMutableStateOf
 import com.xueh.comm_core.weight.compose.SpacerH
 
 /**
@@ -27,8 +30,12 @@ fun PermissionPageContent() {
         listOf(android.Manifest.permission.CAMERA, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
     )
 
+    var singlePermissionStr by rememberMutableStateOf(value = "申请单个权限")
+    var multiplePermissionStr by rememberMutableStateOf(value = "申请多个权限")
+
     when (cameraPermissionState.status) {
         PermissionStatus.Granted -> {//已授权
+            singlePermissionStr="申请单个权限-已授权"
             ToastUtils.showShort("已授权")
         }
 
@@ -37,8 +44,10 @@ fun PermissionPageContent() {
                 //如果用户拒绝了该权限但可以显示理由，那么请温和地解释为什么应用程序需要此权限(拒绝权限)
 //                "The camera is important for this app. Please grant the permission."
                 ToastUtils.showShort("拒绝权限")
+                singlePermissionStr="申请单个权限-拒绝"
             } else {
                 ToastUtils.showShort("用户选择拒绝且不再询问")
+                singlePermissionStr="申请单个权限-拒绝且不再询问"
                 //如果这是用户第一次登陆此功能，或者用户不想再次被要求获得此权限，请说明该权限是必需的(用户选择拒绝且不再询问)
 //                "Camera permission required for this feature to be available. " +
 //                        "Please grant the permission"
@@ -48,14 +57,15 @@ fun PermissionPageContent() {
 
     if (multiplePermissionState.allPermissionsGranted) {
         ToastUtils.showShort("用户全部允许")
+        multiplePermissionStr="申请多个权限-全部允许"
     }
 
     Column {
-        itemView("申请单个权限", showimg = false) {
+        itemView(singlePermissionStr, showimg = false) {
             cameraPermissionState.launchPermissionRequest()
         }
         SpacerH(int = 10)
-        itemView("申请多个权限", showimg = false) {
+        itemView(multiplePermissionStr, showimg = false) {
             multiplePermissionState.launchMultiplePermissionRequest()
         }
     }
