b33b3e946615c63eccd444b92b32aa89513608e8
xuehao <xuehao@deepway.ai>
2023-08-30 17:11:03 +0800

    优化代码

--- buildSrc/src/main/java/buildsrc/Deps.kt
+++ buildSrc/src/main/java/buildsrc/Deps.kt
@@ -3,7 +3,7 @@ package buildsrc
 
 object Deps {
     object Version {
-        const val compose = "1.4.0-alpha03"
+        const val compose = "1.6.0-alpha04"
         const val kotlin = "1.9.0"
         const val coroutines = "1.7.3"
         const val compose_compiler = "1.5.2"
