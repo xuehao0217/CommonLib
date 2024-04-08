626a4203c6f033be5233e3258a2f8f4bc3415a0c
x <644738532@qq.com>
2024-04-08 18:36:43 +0800

    升级kts

---
+++ settings.gradle.kts
@@ -0,0 +1,39 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+
+        maven { url = uri("https://maven.aliyun.com/repository/public/") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://developer.huawei.com/repo/") }
+        maven { url = uri("https://repo1.maven.org/maven2/") }
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+
+        maven { url = uri("https://maven.aliyun.com/repository/public/") }
+        maven { url = uri("https://maven.aliyun.com/repository/central") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://developer.huawei.com/repo/") }
+        maven { url = uri("https://repo1.maven.org/maven2/") }
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+include(":app", ":common_core")
+rootProject.name = "CommonLib"
