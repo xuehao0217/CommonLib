e19cde9a9fcc3a240ce178542e2c4e1dad911701
x <644738532@qq.com>
2024-04-08 17:08:52 +0800

    升级agp

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+//import buildsrc.*
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+}
