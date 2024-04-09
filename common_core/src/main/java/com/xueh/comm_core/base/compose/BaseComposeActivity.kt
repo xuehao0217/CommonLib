8e096aa2ed44501b669b9728f358069737106311
x <644738532@qq.com>
2024-04-09 11:19:42 +0800

    升级kts

--- common_core/src/main/java/com/xueh/comm_core/base/compose/BaseComposeActivity.kt
+++ common_core/src/main/java/com/xueh/comm_core/base/compose/BaseComposeActivity.kt
@@ -4,23 +4,29 @@ import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.core.view.WindowCompat
+import com.xueh.comm_core.base.compose.theme.appThemeState
 import com.xueh.comm_core.weight.ViewLoading
 
 //ComponentActivity
 //AppCompatActivity  可以解决弹窗问题
 abstract class BaseComposeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         val controller = WindowCompat.getInsetsController(window, window.decorView)
-        controller?.isAppearanceLightStatusBars = !(resources.configuration.uiMode == 0x21)
+        controller.isAppearanceLightStatusBars = resources.configuration.uiMode != 0x21
         setContent {
+            appThemeState = appThemeState.copy(
+                darkTheme = isSystemInDarkTheme()
+            )
             setComposeContent()
         }
     }
+
     @Composable
     abstract fun setComposeContent()
     protected fun showProgressDialog() {
