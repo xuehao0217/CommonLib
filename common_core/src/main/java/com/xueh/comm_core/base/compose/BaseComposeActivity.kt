29a844cd3b65aa416ba61d1b820b0e453c82fe27
x <644738532@qq.com>
2024-05-11 13:48:06 +0800

    修改版本

--- common_core/src/main/java/com/xueh/comm_core/base/compose/BaseComposeActivity.kt
+++ common_core/src/main/java/com/xueh/comm_core/base/compose/BaseComposeActivity.kt
@@ -2,24 +2,46 @@ package com.xueh.comm_core.base.compose
 
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.core.view.WindowCompat
 import com.xueh.comm_core.base.compose.theme.appThemeState
+import com.xueh.comm_core.utils.compose.setStatusBarColor
 import com.xueh.comm_core.weight.ViewLoading
 
 //ComponentActivity
 //AppCompatActivity  可以解决弹窗问题
-abstract class BaseComposeActivity : AppCompatActivity() {
+abstract class BaseComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
         super.onCreate(savedInstanceState)
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-        val controller = WindowCompat.getInsetsController(window, window.decorView)
-        controller.isAppearanceLightStatusBars = resources.configuration.uiMode != 0x21
+        enableEdgeToEdge()
         setContent {
+            DisposableEffect(Unit) {
+                enableEdgeToEdge(
+                    statusBarStyle = SystemBarStyle.auto(
+                        android.graphics.Color.TRANSPARENT,
+                        android.graphics.Color.TRANSPARENT,
+                    ) { false }, //这里的意思是是否需要检测深色主题模式，我们使用自己的背景，所以不需要直接设置为false,下面也是一样的
+                    navigationBarStyle = SystemBarStyle.auto(
+                        android.graphics.Color.TRANSPARENT,
+                        android.graphics.Color.TRANSPARENT,
+                    ) { false },
+                )
+                onDispose {}
+            }
             setComposeContent()
         }
     }
