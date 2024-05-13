16f0ff0b67744fe28f8733aa14b66972fe0fba4b
x <644738532@qq.com>
2024-05-13 18:20:58 +0800

    自定义打包

--- pgyer_upload.sh
+++ pgyer_upload.sh
@@ -1,37 +1,55 @@
 #!/bin/bash
 
-# 设置蒲公英平台的API Key和应用ID
-API_KEY="your_api_key"
-APP_ID="your_app_id"
+# Pgyer API key
+PGYER_API_KEY="3623f086318dbb911bb39e5b42a39d11"
 
-# 获取当前模式
-MODE="$1"
+# Pgyer user key
+PGYER_USER_KEY="e0594cc27039c10e449ba15d16ec3ab8"
 
-# 检查模式是否有效
-if [[ "$MODE" != "debug" && "$MODE" != "release" && "$MODE" != "online" ]]; then
-  echo "无效的模式：$MODE"
-  echo "请使用以下模式之一：debug, release, online"
+# 默认构建类型为 debug，可通过命令行参数覆盖
+BUILD_TYPE=${1:-debug}
+
+# 应用名称
+APP_NAME="commonlib"
+
+# 询问用户选择构建类型
+read -p "请选择构建类型（输入1为debug，2为release）: " BUILD_TYPE_CHOICE
+
+case $BUILD_TYPE_CHOICE in
+  1)
+    BUILD_TYPE="debug"
+    ;;
+  2)
+    BUILD_TYPE="release"
+    ;;
+  *)
+    echo "无效输入，将使用默认值debug。"
+    BUILD_TYPE="debug"
+    ;;
+esac
+
+# 提示用户输入更新描述，允许直接回车跳过输入
+read -p "请输入更新描述（可选）: " updateDescription
+updateDescription=${updateDescription:-"默认更新描述"}
+
+# 构建应用
+echo "开始构建 ${BUILD_TYPE} 版本..."
+./gradlew assemble${BUILD_TYPE}
+
+# 设置APK文件所在目录
+APK_DIR="./app/build/outputs/apk/$BUILD_TYPE/"
+
+# 查找APK文件并上传到蒲公英
+APK_FILE=$(find "$APK_DIR" -maxdepth 1 -name "*.apk" 2>/dev/null)
+
+if [ ! -f "$APK_FILE" ]; then
+  echo "未找到APK文件!"
   exit 1
 fi
 
-# 使用Gradle构建工具打包应用
-./gradlew assemble$MODE
+echo "构建完成 ${APK_FILE}，开始上传到蒲公英..."
 
-# 签名应用
-./gradlew signingReport
-
-# 上传应用包到蒲公英平台
-if [[ "$MODE" == "release" || "$MODE" == "online" ]]; then
-  curl -F "file=@app/build/outputs/apk/$MODE/app-$MODE.apk" -F "uKey=$API_KEY" -F "_api_key=$API_KEY" https://www.pgyer.com/apiv2/app/upload
-fi
-
-# 输出应用路径
-echo "应用已打包并签名，路径为：app/build/outputs/apk/$MODE/app-$MODE.apk"
-
-
-
-
-
-#sh pgyer_upload.sh debug
-#sh pgyer_upload.sh release
-#sh pgyer_upload.sh online
+# 上传至蒲公英并传递更新描述
+curl -F "file=@$APK_FILE" -F "uKey=$PGYER_USER_KEY" -F "_api_key=$PGYER_API_KEY" -F "updateDescription=$updateDescription" https://www.pgyer.com/apiv1/app/upload
+echo
+echo "---------------------上传完成---------------------"
