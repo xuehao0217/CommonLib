f82e46d4dc3ffc2cc209cfcd12b7aec93d0e514a
x <644738532@qq.com>
2024-06-19 17:30:03 +0800

    优化代码

--- common_core/src/main/java/com/xueh/comm_core/base/compose/BaseComposeActivity.kt
+++ common_core/src/main/java/com/xueh/comm_core/base/compose/BaseComposeActivity.kt
@@ -34,11 +34,11 @@ abstract class BaseComposeActivity : ComponentActivity() {
                     statusBarStyle = SystemBarStyle.auto(
                         android.graphics.Color.TRANSPARENT,
                         android.graphics.Color.TRANSPARENT,
-                    ) { true }, //这里的意思是是否需要检测深色主题模式，我们使用自己的背景，所以不需要直接设置为false,下面也是一样的
+                    ) { false }, //这里的意思是是否需要检测深色主题模式，我们使用自己的背景，所以不需要直接设置为false,下面也是一样的
                     navigationBarStyle = SystemBarStyle.auto(
                         android.graphics.Color.TRANSPARENT,
                         android.graphics.Color.TRANSPARENT,
-                    ) { true },
+                    ) { false },
                 )
                 onDispose {}
             }
