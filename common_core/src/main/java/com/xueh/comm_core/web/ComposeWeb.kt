96390cec530277b5777405010c27377a7f3ceb6a
x <xuehao0217@gmail.com>
2024-09-12 14:48:50 +0800

    添加webview

---
+++ common_core/src/main/java/com/xueh/comm_core/web/ComposeWeb.kt
@@ -0,0 +1,56 @@
+package com.xueh.comm_core.web
+
+import android.graphics.Bitmap
+import android.util.Log
+import android.webkit.WebView
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.kevinnzou.web.AccompanistWebViewClient
+import com.kevinnzou.web.LoadingState
+import com.kevinnzou.web.WebView
+import com.kevinnzou.web.rememberWebViewNavigator
+import com.kevinnzou.web.rememberWebViewState
+
+@Composable
+fun ComposeWeb (url:String){
+    val state = rememberWebViewState(url = url)
+    val navigator = rememberWebViewNavigator()
+    Column {
+        val loadingState = state.loadingState
+        if (loadingState is LoadingState.Loading) {
+            LinearProgressIndicator(
+                progress = loadingState.progress,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+
+        // A custom WebViewClient and WebChromeClient can be provided via subclassing
+        val webClient = remember {
+            object : AccompanistWebViewClient() {
+                override fun onPageStarted(
+                    view: WebView,
+                    url: String?,
+                    favicon: Bitmap?
+                ) {
+                    super.onPageStarted(view, url, favicon)
+                    Log.d("Accompanist WebView", "Page started loading for $url")
+                }
+            }
+        }
+
+        WebView(
+            state = state,
+            modifier = Modifier
+                .weight(1f),
+            navigator = navigator,
+            onCreated = { webView ->
+                webView.settings.javaScriptEnabled = true
+            },
+            client = webClient
+        )
+    }
+}
