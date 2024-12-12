ea80fd2b48a3747c1f71adb210534147ccfbc164
x <xuehao0217@gmail.com>
2024-12-12 15:48:41 +0800

    修改webview

--- app/src/main/java/com/xueh/commonlib/ui/xml/MyFragment.kt
+++ app/src/main/java/com/xueh/commonlib/ui/xml/MyFragment.kt
@@ -10,7 +10,6 @@ import com.xueh.comm_core.base.xml.DFragment
 import com.xueh.comm_core.helper.activityresult.CropImageActivityResul
 import com.xueh.comm_core.helper.activityresult.CropImageResult
 import com.xueh.comm_core.helper.activityresult.TakeCameraUri
-import com.xueh.comm_core.web.WebViewComposeActivity
 import com.xueh.commonlib.databinding.FragmentMyBinding
 import java.util.concurrent.TimeUnit
 
@@ -79,7 +78,6 @@ class MyFragment : DFragment<FragmentMyBinding>() {
                 interval.resume()
             }
             tvWeb.setOnClickListener {
-                WebViewComposeActivity.start("https://www.baidu.com/")
 //                WebViewActivity.start("https://www.baidu.com/")
             }
         }
